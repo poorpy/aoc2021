55a867efa3e645b4a90138a415c84183af6bc544
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2021-12-01 22:38:54 +0100

    day1: use iterators and slices

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -11,34 +11,28 @@ fn main() {
     )
 }
 
-fn count_increases(measurements: Vec<u64>) -> u64 {
-    let mut acc: u64 = 0;
-    for (index, &item) in measurements.iter().enumerate() {
-        if index + 1 < measurements.len() && item < measurements[index + 1] {
-            acc += 1
-        }
-    }
-
-    acc
+fn count_increases(measurements: Vec<i32>) -> i32 {
+    measurements
+        .iter()
+        .zip(measurements[1..].iter())
+        .map(|(first, second)| if first < second { 1 } else { 0 })
+        .sum()
 }
 
-fn sliding_window(measurements: Vec<u64>, window_size: usize) -> Vec<u64> {
-    let mut acc: Vec<u64> = Vec::new();
-    for (index, _) in measurements.iter().enumerate() {
-        if index + window_size <= measurements.len() {
-            acc.push(measurements[index..index + window_size].iter().sum());
-        }
-    }
-    acc
+fn sliding_window(measurements: Vec<i32>, window_size: usize) -> Vec<i32> {
+    measurements
+        .windows(window_size)
+        .map(|x| x.iter().sum())
+        .collect()
 }
 
-fn read_measurements(filename: &str) -> Vec<u64> {
+fn read_measurements(filename: &str) -> Vec<i32> {
     let contents = fs::read_to_string(filename).expect(&format!(
         "Something went wrong reading the file: {}",
         filename
     ));
     contents
         .split_whitespace()
-        .map(|x| x.parse::<u64>().unwrap())
+        .map(|x| x.parse::<i32>().unwrap())
         .collect()
 }
